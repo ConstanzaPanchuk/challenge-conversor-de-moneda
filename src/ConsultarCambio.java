ac439ab7ae9a3659d89adf9041d3f2c46b403fea
ConstanzaPanchuk <constanza.panchuksteinmann@gmail.com>
2026-02-02 23:46:27 -0300

    conexión con la api

--- src/ConsultarCambio.java
+++ src/ConsultarCambio.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultarCambio {
+
+    Moneda consultarCambio(String monedaBase, String monedaTarget){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/86a83205f8687efed7846183/pair/"
+        + monedaBase + "/" + monedaTarget);
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+            try {
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                return new Gson().fromJson(response.body(), Moneda.class);
+            } catch (Exception e) {
+                throw new RuntimeException("No es una moneda valida");
+            }
+    }
+
 }
--- src/Moneda.java
+++ src/Moneda.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
-public record Moneda() {
+public record Moneda(String base_code,
+                     String target_code,
+                     double conversion_rate) {
 }
